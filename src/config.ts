3aa0846dd56bf213599a568021e54354fed6c0c8
Shaotw1230 <3262818908@qq.com>
2026-05-07 22:26:01 +0800

    stw

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "0xh3y3's Blog",
+	title: "whyulooksad's Blog",
 	subtitle: "Security Research & CTF",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/0xh3y3", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/whyulooksad", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
@@ -54,13 +54,13 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "0xh3y3",
+	name: "whyulooksad",
 	bio: "Security Researcher | CTF Player",
 	links: [
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/0xh3y3",
+			url: "https://github.com/whyulooksad",
 		},
 	],
 };
